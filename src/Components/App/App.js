71f43380d5c38fa91eeb84593b976dd278e5dba6
IrisCaffin <iriscaffin@gmail.com>
2017-12-25 19:58:20 -0800

    jammming steps 11-20
    Steps 11 to 20 out of 100

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import '../SearchBar/SearchBar';
+// import '../SearchResults/SearchResults';
+// import '../Playlist/Playlist';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div class="App">
+          { /* Add a SearchBar component */ }
+          <div class="App-playlist">
+            { /* Add a SearchResults component */ }
+            { /* Add a Playlist component */ }
+          </div>
+        </div>
       </div>
     );
   }
